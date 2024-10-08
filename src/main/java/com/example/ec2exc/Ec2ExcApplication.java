51a09d75dd2a205356210357dd6701bdcd3531fc
muddie <parkjm0817@gmail.com>
2024-10-09 00:31:59 +0900

    feat: project build

---
+++ src/main/java/com/example/ec2exc/Ec2ExcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ec2exc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2ExcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2ExcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ec2exc/Ec2ExcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2exc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2ExcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
